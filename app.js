9aa69844a6a77ed5f73dbeb2f0519c05817b68f2
M Ulmer <mju9999@gmail.com>
2025-01-21 09:43:20 -0800

    Add logic for picking human and computer turn choices

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 "use strict";
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 300) % 3;
+  switch (choice) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+    default:
+      throw new Error("Internal error: failed to parse random number.");
+  }
+}
+
+function getHumanChoice() {
+  let userString;
+  while (
+    userString !== "rock" &&
+    userString !== "paper" &&
+    userString !== "scissors"
+  ) {
+    userString = prompt("Choose your object: rock, paper, or scissors.");
+  }
+}
+
+
